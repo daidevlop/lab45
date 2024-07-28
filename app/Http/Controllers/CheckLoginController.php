06bae2751fcd500fa0770cfeaa7dc16c75634e3e
daidev <cut0266@gmail.com>
2024-07-28 09:17:06 +0700

    cuoi

--- app/Http/Controllers/CheckLoginController.php
+++ app/Http/Controllers/CheckLoginController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckLoginController extends Controller
 {
     public function index(){
         return view('Auths.CheckLogin');
+    }
+    public function postLogin(Request $req){
+        if(Auth::attempt(['email'=>$req->email])){
+            return redirect()->route('account.index');
+        }
     }
 }
